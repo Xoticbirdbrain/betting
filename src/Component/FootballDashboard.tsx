0e01172b1e53cb67fcb3e2d0958cbdc08cef374d
Luke <jspandill@gmail.com>
2026-08-13 15:04:58 +0200

    removed unused useState

--- src/Component/FootballDashboard.tsx
+++ src/Component/FootballDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+
 import { useQuery } from '@tanstack/react-query';
 import { footballDashboardHub } from '@/api/endpoints';
 import type {GetGlobalMatchesParams} from '@/api/types'
